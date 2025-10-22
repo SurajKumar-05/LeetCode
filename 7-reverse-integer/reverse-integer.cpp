d5b85ec6ffefdff4179416f93fd2c244e4f3e7d9
Suraj Kumar <152326864+SurajKumar-05@users.noreply.github.com>
2025-10-22 11:45:59 +0530

    Time: 5 ms (19.90%) | Memory: 8.6 MB (53.90%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int reverse(int x) {
+    int rev = 0; 
+    while (x != 0) {
+        int pop = x % 10;  // Get the last digit
+        x /= 10;           // Remove last digit from x
+
+        // Check for overflow before it happens
+        if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+        if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+
+        rev = rev * 10 + pop;
+    }
+
+    return rev;
+}   
+};
